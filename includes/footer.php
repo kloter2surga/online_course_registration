b7a0249f22ba24f8d3fed69ed3d66ecc4a986cb7
khaira nabila <75076265+khairanabila@users.noreply.github.com>
2021-01-04 07:02:34 +0700

    footer (#10)
    * footer
    
    * Update footer.php

---
+++ includes/footer.php
@@ -0,0 +1,10 @@
+<footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    &copy; 2021 online course | By : <a href="https://github.com/kloter2surga" target="_blank">kloter 2 surga</a>
+                </div>
+
+            </div>
+        </div>
+    </footer>
